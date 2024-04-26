d7afc3f77169c8ddc2caa52d8a1248aa700e9f18
Matthew Luo <mluo31@asu.edu>
2024-04-25 23:55:49 -0700

    first python program created

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is the main program
+
+print("hello there")
+print("right now this is a very simple program")
+
+while True:
+    user_input = input("Please enter a number: ")
+
+    if user_input.isdigit():
+        user_num = int(user_input)
+        
+        if user_num < 100 and user_num >= 0 :
+            break
+        else:
+            print("Number is too high")
+    else:
+        print("hmmmm, I don't recognize that number")
+         
+print("I can count to you number")
+
+for i in range(user_num + 1):
+    print(i)
+
